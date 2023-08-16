6552ed4cad62775a6e1ffd93fac28c517476821d
JustinWang-BOAM <justinwebdev325@gmail.com>
2023-08-16 18:22:03 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
-import './App.css';
-
+import GameComponent from "./Component/GameComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/:hashdata" element={<GameComponent />} />
+      </Routes>
+    </Router>
   );
 }
 
