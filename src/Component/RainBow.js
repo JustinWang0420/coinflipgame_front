be6366cf11ad126a7f61cd9183ee94ff9803a602
JustinWang-BOAM <justinwebdev325@gmail.com>
2023-09-06 14:38:36 +0900

    Update RainBow.js

--- src/Component/RainBow.js
+++ src/Component/RainBow.js
@@ -3,12 +3,11 @@ import { getAccount, parseEther, Account, Chain, Transport, WalletClient } from 
 import { useAccount, useContractRead, useContractReads, useAllowance, useWaitForTransaction } from "wagmi";
 import { useEffect, useState } from "react";
 import Button from '@mui/material/Button';
-import { testABI } from "../ContractABI/testABI";
 import { useParams } from "react-router-dom";
 import Web3 from "web3";
 import './styles.css'; // Make sure to import your CSS file
 import { formatBalance, formatChainAsNum } from './utils'
-// import { waitForTransaction } from "@wagmi/core"
+
 
 const { createWalletClient, custom, createPublicClient, http, providers, Wallet } = require('viem');
 
@@ -25,12 +24,12 @@ const publicClient = createPublicClient({
 const { CoinFlipGameABI } = require("../ContractABI/CoinFlipGameContract_ABI.js")
 const { StandardTokenABI } = require("../ContractABI/StandardToken_ABI.js")
 
-const contractAddress = process.env.REACT_APP_TOKEN_ADDRESS;
 const tokenContractAddress = process.env.REACT_APP_TOKEN_ADDRESS;
 const gameContractAddress = process.env.REACT_APP_GAME_ADDRESS;
 const uniswapUrl = 'https://app.uniswap.org/#/swap?outputCurrency=0xc32db1d3282e872d98f6437d3bcfa57801ca6d5c';
 
 const RainBow = () => {
+    const { isConnected, address } = useAccount();
     const provider = walletClient;
     const Instance = new Web3(provider);
     const gameContractInstance = new Instance.eth.Contract(CoinFlipGameABI, gameContractAddress);
@@ -41,47 +40,52 @@ const RainBow = () => {
     const { userId } = useParams();
     const [betAmount, setBetAmount] = useState(0);
     const [approvedAmount, setApprovedAmount] = useState(0);
-    const { isConnected, address } = useAccount();
     let playgameValue;
+
+
     useEffect(() => {
-        // console.log("address.tokenBalance ::: ", address.tokenBalance)
-        const betAmountValuePromise = gameContractInstance.methods.betAmount().call();
-        betAmountValuePromise.then((betAmountValue) => {
-            const betAmountValueString = betAmountValue.toString();
-            // console.log("betamount ::: ", betAmountValueString * 10);
-            setBetAmount(betAmountValueString * 10);
-        });
-        const _tokenBalance = tokenContractInstance.methods.balanceOf(address).call();
-        _tokenBalance.then((tokenvalue) => {
-            const tokenamountValueString = tokenvalue.toString();
-            // console.log("_tokenbalance ::: ", tokenamountValueString)
-            setTokenBalence(tokenamountValueString);
-        })
-        // console.log("_tokenbalance ::: ", _tokenBalance);
-        // setBetAmount(betAmountValue.toString());
-        // console.log("betamount ::: ", betAmountValue.toString(), betAmount)
-        // console.log("provider", Instance);
-        // console.log(userId);
         async function fetchAddresses() {
             if (walletClient && walletClient.getAddresses) {
                 const addresses = await walletClient.getAddresses();
                 setClientAddress(addresses[0]);
             }
         }
-        // console.log("ApprovalResponse :: ", ApprovalResponse)
         fetchAddresses();
-    }, [tokenBalance]);
+        if (address) {
+            const betAmountValuePromise = gameContractInstance.methods.betAmount().call();
+            console.log("betAmountValuePromise :::: ", betAmountValuePromise)
+            betAmountValuePromise.then((betAmountValue) => {
+                const betAmountValueString = betAmountValue.toString();
+                console.log("betamount ::: ", betAmountValueString);
+                setBetAmount(betAmountValueString);
+            });
+            const _tokenBalance = tokenContractInstance.methods.balanceOf(address).call();
+            _tokenBalance.then((tokenvalue) => {
+                const tokenamountValueString = tokenvalue.toString();
+                console.log("_tokenbalance ::: ", tokenamountValueString)
+                setTokenBalence(tokenamountValueString);
+            })
+            console.log("_tokenbalance ::: ", _tokenBalance);
+            // setBetAmount(betAmountValue.toString());
+            // console.log("betamount ::: ", betAmountValue.toString(), betAmount)
+            console.log("provider", Instance);
+            console.log(userId);
+        }
+    }, []);
     async function Transaction() {
+        console.log('Transaction==============')
         const account = getAccount(clientAddress);
         const spenderAddress = gameContractAddress;
         const amount = betAmount;
+        console.log("amount ", amount, " spenderAddress ::: ", spenderAddress);
+        const gasLimit = 500000;
         const approvedAmountPromise = tokenContractInstance.methods.allowance(address, gameContractAddress).call();
         approvedAmountPromise.then((approvedAmountvalue) => {
             const approvedAmountString = approvedAmountvalue.toString();
-            // console.log("approvedAmountString ::: ", approvedAmountString, " betAmountValueString ::: ", betAmount);
+            console.log("approvedAmountString ::: ", approvedAmountString, " betAmountValueString ::: ", betAmount);
             setApprovedAmount(approvedAmount);
         });
-        // console.log("approvedAmount ::: ", approvedAmount, "betAmount ::: ", betAmount.fulfilled);
+        console.log("approvedAmount ::: ", approvedAmount, "betAmount ::: ", betAmount.fulfilled);
         try {
             const approveValue = await walletClient.writeContract({
                 address: tokenContractAddress,
@@ -90,6 +94,12 @@ const RainBow = () => {
                 args: [spenderAddress, amount],
                 account,
             });
+            // const approveValue = await tokenContractInstance.methods.approve(spenderAddress, amount).send({ from: address, gas: gasLimit });
+            // console.log("approveValue ::: ", approveValue);
+            // .then((approvevalue) => {
+            //     playgameValue = gameContractInstance.methods.playGame(userId).send({ from: address, gas: gasLimit });
+            // })
+            // console.log(approveValue);
             await new Promise(resolve => setTimeout(resolve, 13000));
             walletClient.writeContract({
                 address: gameContractAddress,
@@ -108,8 +118,8 @@ const RainBow = () => {
             <ConnectButton />
             {address && (
                 <>
-                    <p className="balance" style={{ color: "black " }}>Bet Amount:{formatBalance(betAmount)}</p>
-                    {!parseInt(tokenBalance) ? <p className="balance" style={{ color: "black " }}>Token Amount : 0</p> : <p className="balance" style={{ color: "black " }}>Token Amount : {parseInt(tokenBalance)}</p>}
+                    <p className="balance" style={{ color: "black " }}>Bet Amount:{betAmount}</p>
+                    {!parseInt(tokenBalance) ? <p className="balance" style={{ color: "black " }}>Token Amount : 0</p> : <p className="balance" style={{ color: "black " }}>Token Amount : {tokenBalance}</p>}
 
                     {parseInt(tokenBalance) > formatBalance(betAmount) ? <Button onClick={Transaction} variant="outline" color="black" style={{ backgroundColor: 'black' }}>Scroto</Button> :
                         <a href={uniswapUrl} style={{ color: "white" }} target="_blank">
@@ -124,5 +134,3 @@ const RainBow = () => {
     );
 };
 export default RainBow;
-
-
